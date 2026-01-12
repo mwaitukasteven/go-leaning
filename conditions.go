3954d5d7a52edc071e157f701f40620eb8ae66b4
mwaitukasteven <mwaitukasteven@gmail.com>
2026-01-12 23:36:22 +0300

    learning go

--- conditions.go
+++ conditions.go
@@ -1,5 +1,12 @@
-if age >= 18 {
-	fmt.Println("Adult")
-} else {
-	fmt.Println("Minor")
+package main
+
+import "fmt"
+
+func main() {
+	age := 20
+	if age >= 18 {
+		fmt.Println("Adult")
+	} else {
+		fmt.Println("Minor")
+	}
 }
