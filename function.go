970d30bde60b086dd7713230bbc997d6f153f151
Mwaituka_tech <166612308+mwaitukasteven@users.noreply.github.com>
2026-01-13 23:58:07 +0300

    Remove division function error handling

--- function.go
+++ function.go
@@ -5,8 +5,4 @@ func multiply(a int, b int) int {
 	return a * b
 }
 func divide(a int, b int) (int, error) {
-	if b == 0 {
-		return 0, fmt.Errorf("division by zero")
-	}
-	return a / b, nil
-}package main
+
