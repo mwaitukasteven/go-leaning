97faa346c6e7f1da7f83697f7f6a1bf86be46396
mwaitukasteven <mwaitukasteven@gmail.com>
2026-01-17 23:26:26 +0300

    added

--- loops.go
+++ loops.go
@@ -1,3 +1,39 @@
-for i := 0; i < 5; i++ {
-	fmt.Println(i)
+package main
+
+import "fmt"
+
+func look() {
+
+	for i := 1; i <= 5; i++ {
+		fmt.Println("Number:", i)
+	}
+
+}
+
+
+func mai() {
+
+	i := 1
+
+	for {
+		fmt.Println("Looping:", i)
+		i++
+
+		if i > 5 {
+			break
+		}
+	}
+
+}
+
+
+func ma() {
+
+	count := 1
+
+	for count <= 5 {
+		fmt.Println("Count:", count)
+		count++
+	}
+
 }
