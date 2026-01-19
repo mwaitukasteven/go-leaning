87ab18d2c50587104717d7a6532283cedc9d60e7
mwaitukasteven <mwaitukasteven@gmail.com>
2026-01-20 01:41:08 +0300

    learn loops in maps

---
+++ map_loops.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func names() {
+
+	users := map[string]string{
+		"admin": "Steven",
+		"user":  "Amina",
+	}
+
+	for key, value := range users {
+		fmt.Println(key, "=>", value)
+	}
+}
